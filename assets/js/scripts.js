c942392ff882a3890ad150510bca001090ad6ea3
clevertonalmeida <117084721+clevertonalmeida@users.noreply.github.com>
2023-10-04 08:39:21 -0300

    add funciont select winner

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,15 +1,29 @@
 "use strict";
 
 let cells = [];
-let CLOSECLASSNAME = 'close';
-let CIRCLECLASSNAME = 'circle';
+const PLAYER_X_CLASS = 'x';
+const PLAYER_O_CLASS = 'o';
+
+const WINNING_COMBINATIONS = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6]
+];
+
 let btnReset = document.querySelector(".btnReset");
 
 function loadTable() {
- 
+
   const tbElement = document.createElement('table');
   const tbBodyElement = document.createElement('tbody');
   const appBody = document.querySelector('.app-body');
+
+  let count = 0;
 
   for (let tr = 0; tr < 3; tr++) {
 
@@ -19,7 +33,11 @@ function loadTable() {
 
       const cell = document.createElement("td");
 
+      cell.dataset.cell=count;
+
       row.appendChild(cell);
+
+      count++;
 
     }
 
@@ -32,16 +50,31 @@ function loadTable() {
 
 }
 
-function resetTable(){
+function checkWin(currentClass) {
+
+ for(let i = 0; i < WINNING_COMBINATIONS.length; i++){
+    
+    let win =[];
+    
+    for(let j = 0; j < 3; j++){
+       if (tableCells[WINNING_COMBINATIONS[i][j]].classList.contains(currentClass)){
+        win.push(j);
+       }
+    }
+  
+    if (win.length == 3)
+       return true;
+
+  }
+}
+
+function resetTable() {
 
   cells = [];
-  
-  document.querySelectorAll('.app-body td')
-      .forEach(e => e.classList.remove(CLOSECLASSNAME));
 
   document.querySelectorAll('.app-body td')
-    .forEach(e => e.classList.remove(CIRCLECLASSNAME));
-
+    .forEach(e => e.classList.remove(PLAYER_X_CLASS)
+      || e.classList.remove(PLAYER_O_CLASS));
 }
 
 loadTable();
@@ -50,27 +83,24 @@ btnReset.onclick = resetTable;
 document.querySelectorAll('.app-body td')
   .forEach(e => e.addEventListener("click", function (e) {
 
+    const cellNewValue = cells.length > 0 && cells[cells.length - 1].cellValue == PLAYER_X_CLASS
+        ? PLAYER_O_CLASS
+        : PLAYER_X_CLASS;
+
     if (cells.length == 10
-      || this.className.includes(CLOSECLASSNAME)
-      || this.className.includes(CIRCLECLASSNAME)) {
+      || this.className.includes(PLAYER_X_CLASS)
+      || this.className.includes(PLAYER_O_CLASS)) {
       return;
     }
 
-    if (cells.length == 0) {
+    this.classList.add(cellNewValue);
+        
+     cells.push({ cellValue: cellNewValue,  pos:this.getAttribute("data-cell") });
 
-      this.classList.add(CLOSECLASSNAME);
-      cells.push(CLOSECLASSNAME);
-
-    } else {
-
-      const cellLastValue = cells[cells.length - 1] == CLOSECLASSNAME
-        ? CIRCLECLASSNAME
-        : CLOSECLASSNAME;
-
-      this.classList.add(cellLastValue);
-
-      cells.push(cellLastValue);
+    if (checkWin(cellNewValue)){
+      document.querySelector('.winner').innerHTML = cellNewValue;
     }
+
   }));
 
-
+  var tableCells = document.querySelectorAll('.app-body td');
